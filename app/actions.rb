9d0063d8e4b7a9d4c6cb1df2272fc4789453012c
Susie Ehrhardt <susan.e.ehrhardt@gmail.com>
2016-10-13 02:09:13 +0000

    templating with ruby

--- app/actions.rb
+++ app/actions.rb
@@ -5,18 +5,14 @@ def humanized_time_ago(time_ago_in_minutes)
         "#{time_ago_in_minutes} minutes ago"
     end
 end
-
-get '/images/sharky_j.jpg' do
-  "@post_shark = avatar_url"
-end
   
 
 get '/' do
     
     @post_shark = {
         username: "sharky_j",
-        avatar_url: "/images/sharky_j.jpg",
-        photo_url: "/images/shark.png",
+        avatar_url: "http://naserca.com/images/sharky_j.jpg",
+        photo_url: "http://naserca.com/images/shark.png",
         humanized_time_ago: humanized_time_ago(15),
         like_count: 0,
         comment_count: 1,
@@ -28,8 +24,8 @@ get '/' do
     
     @post_whale = {
         username: "kirk_whalum",
-        avatar_url: "/images/kirk_whalum.jpg",
-        photo_url: "/images/whale.jpg",
+        avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
+        photo_url: "http://naserca.com/images/whale.jpg",
         humanized_time_ago: humanized_time_ago(65),
         like_count: 0,
         comment_count: 1,
@@ -41,8 +37,8 @@ get '/' do
     
     @post_marlin = {
         username: "marlin_peppa",
-        avatar_url: "/images/marlin_peppa.jpg",
-        photo_url: "/images/marlin.jpg",
+        avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
+        photo_url: "http://naserca.com/images/marlin.jpg",
         humanized_time_ago: humanized_time_ago(190),
         like_count: 0,
         comment_count: 1,
@@ -54,6 +50,7 @@ get '/' do
     
     [@post_shark, @post_whale, @post_marlin].to_s
     
-    erb(:index)
+     @posts = [@post_shark, @post_whale, @post_marlin]
+
+    erb :index
 end
- 
